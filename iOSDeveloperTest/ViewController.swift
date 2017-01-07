9c247b2d532b1b905b8a1b50a14c99fe88506eb8
Ashish Kumar Sai <ashishkumarsai@me.com>
2017-01-07 13:38:40 +0530

    Initial Commit

---
+++ iOSDeveloperTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSDeveloperTest
+//
+//  Created by Ashutosh Kumar Sai on 07/01/17.
+//  Copyright © 2017 Ashish Rajendra Kumar Sai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
